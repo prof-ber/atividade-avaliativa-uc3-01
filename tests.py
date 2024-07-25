6a3488d08015aed40d2005d6a99c6130ae88f838
mauvemagician <>
2024-07-25 16:46:26 -0300

    Add tests, code

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from main import factorial
+from main import factorial, fibonacci, gen_fibonacci
 
 class TestFactorial(unittest.TestCase):
     def test_factorial(self):
@@ -10,6 +10,18 @@ class TestFactorial(unittest.TestCase):
         self.assertEqual(factorial(3), 6)
         self.assertEqual(factorial(4), 24)
         self.assertEqual(factorial(5), 120)
+
+    def test_fibonacci(self):
+        self.assertEqual(fibonacci(0), 0)
+        self.assertEqual(fibonacci(1), 1)
+        self.assertEqual(fibonacci(2), 1)
+        self.assertEqual(fibonacci(3), 2)
+        self.assertEqual(fibonacci(4), 3)
+        self.assertEqual(fibonacci(5), 5)
+
+    def test_gen_fibonacci(self):
+        fib_sequence = list(gen_fibonacci(10))
+        self.assertEqual(fib_sequence, [0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
 
 if __name__ == '__main__':
     unittest.main()
