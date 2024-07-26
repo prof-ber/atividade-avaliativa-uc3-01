68e98094dd3ef9a040bf8201cebe45e75356ba6c
bernardo <bernardo.lima@docente.pr.senac.br>
2024-07-25 21:46:40 -0300

    Fix code

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def factorial(n):
     if n == 0:
-        return 0
+        return 1
     else:
         return n * factorial(n-1)
 
 def fibonacci(n):
     if n <= 1:
-        return 0
+        return n
     else:
         return fibonacci(n-1) + fibonacci(n-2)
 
@@ -14,4 +14,4 @@ def gen_fibonacci(n):
     a, b = 0, 1
     for _ in range(n):
         yield a
-        a, b = a + b, b
+        a, b = b, a + b
